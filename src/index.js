def9657466488393f8539c18119d2e170b50952d
GuilleSilk <storesilkify@gmail.com>
2025-06-18 16:05:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import { generateLicense } from './generate-license';
 import { validateLicense } from './validate-license';
 import { webhookTest } from './webhook-test';
 
-addEventListener('fetch', event => {
-  const request = event.request;
-  const url = new URL(request.url);
-
-  if (url.pathname === '/generate-license') {
-    event.respondWith(generateLicense(request));
-  } else if (url.pathname === '/validate-license') {
-    event.respondWith(validateLicense(request));
-  } else if (url.pathname === '/webhook-test') {
-    event.respondWith(webhookTest(request));
-  } else {
-    event.respondWith(new Response('Not Found', { status: 404 }));
+export default {
+  // Este método recibe (request, env, ctx)
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === '/generate-license') {
+      return generateLicense(request, env);
+    }
+    if (url.pathname === '/validate-license') {
+      return validateLicense(request, env);
+    }
+    if (url.pathname === '/webhook-test') {
+      return webhookTest(request, env);
+    }
+    return new Response('Not Found', { status: 404 });
   }
-});
+};
+);
